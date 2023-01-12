e3295dffac13419b4e8d3b4db35ab8d03c137dd3
= <=>
2023-01-12 19:22:57 +0530

    ver 002 commit

--- facts_gene/__init__.py
+++ facts_gene/__init__.py
@@ -4,11 +4,13 @@
 from .facts_gen_commons import KeyExchanger, CiscoDB
 from .facts_gen_tables_ntct import TableInterfaceCisco, TableInterfaceJuniper, TableVrfsCisco
 from .facts_gen_var_ntct import VarInterfaceCisco, VarInterfaceJuniper
+from .facts_gen_from_fg import DeviceFactsFg
 
 __all__ = [ 
 	'KeyExchanger', 'CiscoDB',
 	'TableInterfaceCisco', 'TableInterfaceJuniper', 'TableVrfsCisco',
 	'VarInterfaceCisco', 'VarInterfaceJuniper',
+	'DeviceFactsFg',
 	]
 
-__ver__ = "0.0.1"
+__ver__ = "0.0.2"
